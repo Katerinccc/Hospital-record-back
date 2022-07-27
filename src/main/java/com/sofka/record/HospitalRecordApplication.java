81b5b05ccb21efef820684b9963b02ffdcf46d66
Katerin Calderon <kccc0713@gmail.com>
2022-07-27 10:44:32 -0500

    initial project setup

---
+++ src/main/java/com/sofka/record/HospitalRecordApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.record;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/record/HospitalRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.record;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
